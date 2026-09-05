526efbbb5d36d067d8150b0c7015d51491e0614c
kode2025q <anton.pustovit.1993@gmail.com>
2026-09-05 23:51:07 +0400

    Initialize Telegram bot with start command

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.environ["BOT_TOKEN"]
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет!")
+
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущено!")
+
+app.run_polling()
